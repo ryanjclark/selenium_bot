354daf53aa1691f5f3118a97a027747fb840c3bd
ryanjclark <ryanclark2021@u.northwestern.edu>
2020-05-04 10:55:54 -0500

    Added requirements

--- create_environment.sh
+++ create_environment.sh
@@ -1,6 +1,13 @@
-echo "curl command for Chromedriver"
-# Chromedriver needs to be in opt/WebDriver/bin
+echo "Creating virtualenv 'selenium'"
+virtualenv selenium
+source selenium/bin/activate
+
+echo "Installing requirements"
+pip install -r requirements.txt
+
 # TODO: curl chromedriver latest version
+# echo "curl command for Chromedriver"
+# Chromedriver needs to be in opt/WebDriver/bin
 
 echo "Adding WebDrivers to PATH"
 export PATH=$PATH:/opt/WebDriver/bin >> ~/.profile
